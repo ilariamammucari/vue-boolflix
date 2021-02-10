ccf7bbdd7bf546995075b29416d3bbab94611331
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-02-10 14:52:24 +0100

    milestone 1 con voti approssimati

--- vue/script.js
+++ vue/script.js
@@ -1,9 +1,43 @@
 var app = new Vue({
     el: '#root',
     data:{
-        valoreInput: ''
+        valoreInput: '',
+        apiKey: '64ea8cbbcce6f7fd105fce2d2c4b9e9a',
+        lingua: 'it-IT',
+        films: ''
     },
     methods: {
+        ricercaInput(){
+            axios.get("https://api.themoviedb.org/3/search/movie", {
+                params: {
+                    api_key: this.apiKey,
+                    query: this.valoreInput,
+                    language: this.lingua
+                }
+            }).then((result) => {
+                this.films = result.data.results;
+                this.valoreInput = '';
 
+                this.voto();
+            }).catch((error) => alert('errori'));
+
+            axios.get("https://api.themoviedb.org/3/search/tv", {
+                params: {
+                    api_key: this.apiKey,
+                    query: this.valoreInput,
+                    language: this.lingua
+                }
+            }).then((result) => {
+                this.films.push(result.data.results);
+                this.valoreInput = '';
+
+                this.voto();
+            }).catch((error) => alert('errori'));
+        },
+        voto(){
+            this.films.forEach((element) => {
+                element.vote_average = parseInt(element.vote_average * 5 / 10);
+            });
+        }
     }
 });
